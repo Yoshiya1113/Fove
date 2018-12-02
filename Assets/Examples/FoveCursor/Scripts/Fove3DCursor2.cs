5795cc95796795245086908b3eec847f9848c40f
Yoshiya1113 <43697625+Yoshiya1113@users.noreply.github.com>
2018-12-02 19:43:08 +0900

    TunneringMove

---
+++ Assets/Examples/FoveCursor/Scripts/Fove3DCursor2.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fove3DCursor2 : MonoBehaviour {
+    public Material plane;//マテリアルの変数
+    public Vector3 pos;//位置の変数
+    public Vector3 eyepos;//視点の位置の変数
+    public Vector3 tunpos;//トンネリングの位置の変数
+    public Vector3 displaysize;
+
+    // Use this for initialization
+    void Start () {
+        displaysize = new Vector3(25, 14, 1);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        FoveInterface.EyeRays eyes = FoveInterface.GetEyeRays();
+        RaycastHit hitLeft, hitRight;
+
+        switch (FoveInterface.CheckEyesClosed())
+        {
+            case Fove.EFVR_Eye.Neither:
+
+                Physics.Raycast(eyes.left, out hitLeft, Mathf.Infinity);
+                Physics.Raycast(eyes.right, out hitRight, Mathf.Infinity);
+                if (hitLeft.point != Vector3.zero && hitRight.point != Vector3.zero)
+                {
+                    eyepos = hitLeft.point + ((hitRight.point - hitLeft.point) / 2);
+                    //eyepos = eyepos / displaysize;
+                    eyepos.x = eyepos.x / displaysize.x;
+                    eyepos.y = eyepos.y / displaysize.y;
+                    plane.SetFloat("_UX", eyepos.x);//マウスのx座標をシェーダーのx座標に代入
+                    plane.SetFloat("_VY", eyepos.y);//マウスのy座標をシェーダーのx座標に代入
+                }
+                else
+                {
+                    eyepos = eyes.left.GetPoint(3.0f) + ((eyes.right.GetPoint(3.0f) - eyes.left.GetPoint(3.0f)) / 2); ;
+                    //eyepos = eyepos / displaysize;
+                    eyepos.x = eyepos.x / displaysize.x;
+                    eyepos.y = eyepos.y / displaysize.y;
+                    plane.SetFloat("_UX", eyepos.x);//マウスのx座標をシェーダーのx座標に代入
+                    plane.SetFloat("_VY", eyepos.y);//マウスのy座標をシェーダーのx座標に代入
+                }
+
+                break;
+            case Fove.EFVR_Eye.Left:
+
+                Physics.Raycast(eyes.right, out hitRight, Mathf.Infinity);
+                if (hitRight.point != Vector3.zero) // Vector3 is non-nullable; comparing to null is always false
+                {
+                    eyepos = hitRight.point;
+                    //eyepos = eyepos / displaysize;
+                    eyepos.x = eyepos.x / displaysize.x;
+                    eyepos.y = eyepos.y / displaysize.y;
+                    plane.SetFloat("_UX", eyepos.x);//マウスのx座標をシェーダーのx座標に代入
+                    plane.SetFloat("_VY", eyepos.y);//マウスのy座標をシェーダーのx座標に代入
+                }
+                else
+                {
+                    eyepos = eyes.right.GetPoint(3.0f);
+                    //eyepos = eyepos / displaysize;
+                    eyepos.x = eyepos.x / displaysize.x;
+                    eyepos.y = eyepos.y / displaysize.y;
+                    plane.SetFloat("_UX", eyepos.x);//マウスのx座標をシェーダーのx座標に代入
+                    plane.SetFloat("_VY", eyepos.y);//マウスのy座標をシェーダーのx座標に代入
+                }
+                break;
+            case Fove.EFVR_Eye.Right:
+
+                Physics.Raycast(eyes.left, out hitLeft, Mathf.Infinity);
+                if (hitLeft.point != Vector3.zero) // Vector3 is non-nullable; comparing to null is always false
+                {
+                    eyepos = hitLeft.point;
+                    //eyepos = eyepos / displaysize;
+                    eyepos.x = eyepos.x / displaysize.x;
+                    eyepos.y = eyepos.y / displaysize.y;
+                    plane.SetFloat("_UX", eyepos.x);//マウスのx座標をシェーダーのx座標に代入
+                    plane.SetFloat("_VY", eyepos.y);//マウスのy座標をシェーダーのx座標に代入
+                }
+                else
+                {
+                    eyepos = eyes.left.GetPoint(3.0f);
+                    //eyepos = eyepos / displaysize;
+                    eyepos.x = eyepos.x / displaysize.x;
+                    eyepos.y = eyepos.y / displaysize.y;
+                    plane.SetFloat("_UX", eyepos.x);//マウスのx座標をシェーダーのx座標に代入
+                    plane.SetFloat("_VY", eyepos.y);//マウスのy座標をシェーダーのx座標に代入
+                    
+                }
+                break;
+        }
+    }
+}
---
+++ Assets/Examples/FoveCursor/Scripts/Fove3DCursorhit.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fove3DCursorhit : MonoBehaviour {
+    private Vector3 originhit;//視点
+    private float origindirection;//視点までの距離の2乗
+    private float origindirection2;//視点までの距離の
+    private float origindirection3;//視点までの距離の√
+    private Vector3 newtunnering;//トンネリング座標
+    private float root10 = 3.162f;//計算用の√10の値
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        FoveInterface.EyeRays eyes = FoveInterface.GetEyeRays();
+        RaycastHit hitLeft, hitRight;
+
+        switch (FoveInterface.CheckEyesClosed())
+        {
+            case Fove.EFVR_Eye.Neither:
+
+                Physics.Raycast(eyes.left, out hitLeft, Mathf.Infinity);
+                Physics.Raycast(eyes.right, out hitRight, Mathf.Infinity);
+                if (hitLeft.point != Vector3.zero && hitRight.point != Vector3.zero)
+                {
+                    originhit = hitLeft.point + ((hitRight.point - hitLeft.point) / 2);//視点を代入
+                    origindirection = originhit.x * originhit.x + originhit.y * originhit.y + originhit.z * originhit.z;//距離の2乗を計算
+                    origindirection2 = Mathf.Sqrt(origindirection);
+                    origindirection3 = Mathf.Sqrt(origindirection2);
+                    newtunnering = new Vector3(originhit.x * root10 / origindirection3, originhit.y * root10 / origindirection3, originhit.z * root10 / origindirection3);//トンネリングの座標を計算して代入
+                    transform.position = newtunnering;//トンネリングを移動
+                }
+                else
+                {
+                    originhit = eyes.left.GetPoint(3.0f) + ((eyes.right.GetPoint(3.0f) - eyes.left.GetPoint(3.0f)) / 2);//視点を代入
+                    origindirection = originhit.x * originhit.x + originhit.y * originhit.y + originhit.z * originhit.z;//距離の2乗を計算
+                    origindirection2 = Mathf.Sqrt(origindirection);
+                    origindirection3 = Mathf.Sqrt(origindirection2);
+                    newtunnering = new Vector3(originhit.x * root10 / origindirection3, originhit.y * root10 / origindirection3, originhit.z * root10 / origindirection3);//トンネリングの座標を計算して代入
+                    transform.position = newtunnering;//トンネリングを移動
+                }
+                break;
+            case Fove.EFVR_Eye.Left:
+
+                Physics.Raycast(eyes.right, out hitRight, Mathf.Infinity);
+                if (hitRight.point != Vector3.zero) // Vector3 is non-nullable; comparing to null is always false
+                {
+                    originhit = hitRight.point;//視点を代入
+                    origindirection = originhit.x * originhit.x + originhit.y * originhit.y + originhit.z * originhit.z;//距離の2乗を計算
+                    origindirection2 = Mathf.Sqrt(origindirection);
+                    origindirection3 = Mathf.Sqrt(origindirection2);
+                    newtunnering = new Vector3(originhit.x * root10 / origindirection3, originhit.y * root10 / origindirection3, originhit.z * root10 / origindirection3);//トンネリングの座標を計算して代入
+                    transform.position = newtunnering;//トンネリングを移動
+                }
+                else
+                {
+                    originhit = eyes.right.GetPoint(3.0f);//視点を代入
+                    origindirection = originhit.x * originhit.x + originhit.y * originhit.y + originhit.z * originhit.z;//距離の2乗を計算
+                    origindirection2 = Mathf.Sqrt(origindirection);
+                    origindirection3 = Mathf.Sqrt(origindirection2);
+                    newtunnering = new Vector3(originhit.x * root10 / origindirection3, originhit.y * root10 / origindirection3, originhit.z * root10 / origindirection3);//トンネリングの座標を計算して代入
+                    transform.position = newtunnering;//トンネリングを移動
+                }
+                break;
+            case Fove.EFVR_Eye.Right:
+
+                Physics.Raycast(eyes.left, out hitLeft, Mathf.Infinity);
+                if (hitLeft.point != Vector3.zero) // Vector3 is non-nullable; comparing to null is always false
+                {
+                    originhit = hitLeft.point;//視点を代入
+                    origindirection = originhit.x * originhit.x + originhit.y * originhit.y + originhit.z * originhit.z;//距離の2乗を計算
+                    origindirection2 = Mathf.Sqrt(origindirection);
+                    origindirection3 = Mathf.Sqrt(origindirection2);
+                    newtunnering = new Vector3(originhit.x * root10 / origindirection3, originhit.y * root10 / origindirection3, originhit.z * root10 / origindirection3);//トンネリングの座標を計算して代入
+                    transform.position = newtunnering;//トンネリングを移動
+                }
+                else
+                {
+                    originhit = eyes.left.GetPoint(3.0f);//視点を代入
+                    origindirection = originhit.x * originhit.x + originhit.y * originhit.y + originhit.z * originhit.z;//距離の2乗を計算
+                    origindirection2 = Mathf.Sqrt(origindirection);
+                    origindirection3 = Mathf.Sqrt(origindirection2);
+                    newtunnering = new Vector3(originhit.x * root10 / origindirection3, originhit.y * root10 / origindirection3, originhit.z * root10 / origindirection3);//トンネリングの座標を計算して代入
+                    transform.position = newtunnering;//トンネリングを移動
+                }
+                break;
+        }
+
+    }
+}
